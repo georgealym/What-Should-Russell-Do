36deb106922a6790af90704b727ff7bfab3c4bb0
Alyssa George <georgealym@gmail.com>
2020-11-10 20:40:37 -0500

    adding information

---
+++ static/js/data.js
@@ -0,0 +1,75 @@
+var data = {
+    quarter: {
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4
+    },
+
+    down: {
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4
+    },
+
+    defendersInTheBox: {
+        3: 0,
+        4: 1,
+        5: 2,
+        6: 3,
+        7: 4, 
+        8: 5, 
+        9: 6, 
+        10: 7
+    },
+
+    numberOfPassRushers: {
+        2: 0,
+        3: 1,
+        4: 2,
+        5: 3,
+        6: 4, 
+        7: 5, 
+        8: 6
+        },
+
+    playDirection: {
+        Left: 0,
+        Right: 1
+    },
+
+    route: {
+        Angle: 0,
+        Corner: 1,
+        Cross: 2,
+        Flat: 3,
+        Go: 4, 
+        Hitch: 5, 
+        In: 6,
+        Out: 7,
+        Post: 8, 
+        Screen: 9, 
+        Slant: 10,
+        Wheel: 11
+    },
+
+    offenseFormation: {
+        "Designed Roll Out Left": 0,
+        "Designed Roll Out Right": 1,
+        "Scramble": 2,
+        "Scramble Roll Out Left": 3,
+        "Scramble Roll Out Right": 4, 
+        "Traditional": 5
+    },
+
+
+    typeDropback: {
+        "Empty": 0,
+        "I Form": 1,
+        "Jumbo": 2,
+        "Pistol": 3,
+        "Shotgun": 4, 
+        "Single Back": 5
+    }
+};
